0cd160ac3b8b352a0a283489745ab12a365fe724
saberzero1 <github@emilebangma.com>
2026-05-24 20:37:16 +0200

    build: bundle dependencies and ship pre-built dist

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import { createRequire } from 'module';
-import { BasesBody_default, resolveBasesEntries, registerBuiltinViews, i18n, ViewSelector } from './chunk-V6J7BH3B.js';
-export { BasesBody_default as BasesBody } from './chunk-V6J7BH3B.js';
+import { BasesBody_default, resolveBasesEntries, registerBuiltinViews, i18n, ViewSelector } from './chunk-JVCM57YT.js';
+export { BasesBody_default as BasesBody } from './chunk-JVCM57YT.js';
 import { registerCustomViews, viewRegistry } from './chunk-2AUMER56.js';
 export { registerCustomViews, viewRegistry } from './chunk-2AUMER56.js';
 import { slugifyFilePath, k, S, l } from './chunk-4A627OLJ.js';
